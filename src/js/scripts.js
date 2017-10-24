287d886e467bd1e038a7f91217a592beaf7abf71
Sergej Lazaryan <lazaryan99@gmail.com>
2017-10-24 03:17:17 +0300

    add files

---
+++ src/js/scripts.js
@@ -0,0 +1,9 @@
+document.getElementById('label_color_white').onclick = function(){
+	document.getElementById('images_product').innerHTML = '<img src="img/tshirt_white.jpg" alt="" class="product_images">';
+}
+document.getElementById('label_color_yellow').onclick = function(){
+	document.getElementById('images_product').innerHTML = '<img src="img/tshirt_yellow.jpg" alt="" class="product_images">';
+}
+document.getElementById('label_color_green').onclick = function(){
+	document.getElementById('images_product').innerHTML = '<img src="img/tshirt_green.jpg" alt="" class="product_images">';
+}
