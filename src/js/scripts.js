ee320b02293e5110031da40d99f4c7d873791b6f
Sergej Lazaryan <lazaryan99@gmail.com>
2017-10-31 01:27:35 +0300

    copied styles

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,9 +1,10 @@
-document.getElementById('label_color_white').onclick = function(){
-	document.getElementById('images_product').innerHTML = '<img src="img/tshirt_white.jpg" alt="" class="product_images">';
+// Your code here...
+document.getElementById('tshirt_white').onclick = function(){
+	document.getElementById('image__product').innerHTML = '<img src="img/tshirt_white.jpg" alt="" class="product_images">';
 }
-document.getElementById('label_color_yellow').onclick = function(){
-	document.getElementById('images_product').innerHTML = '<img src="img/tshirt_yellow.jpg" alt="" class="product_images">';
+document.getElementById('tshirt_yellow').onclick = function(){
+	document.getElementById('image__product').innerHTML = '<img src="img/tshirt_yellow.jpg" alt="" class="product_images">';
 }
-document.getElementById('label_color_green').onclick = function(){
-	document.getElementById('images_product').innerHTML = '<img src="img/tshirt_green.jpg" alt="" class="product_images">';
+document.getElementById('tshirt_green').onclick = function(){
+	document.getElementById('image__product').innerHTML = '<img src="img/tshirt_green.jpg" alt="" class="product_images">';
 }
