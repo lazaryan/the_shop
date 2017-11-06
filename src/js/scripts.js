844cfa0ed545058976eee60df23bee0cff3b6e31
Sergej Lazaryan <lazaryan99@gmail.com>
2017-11-06 23:05:28 +0300

    mobilna version and js

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,14 +1,37 @@
-// Your code here...
-
 const form = document.querySelector('.js-product-form');
-
 form.addEventListener('click' , (event) => {
-
-	const target = event.target;
-	if(target.classList.contains('js-product-details__size')){
-		console.log(document.getElementsByClassName('js-product-details__size-active'));
-		
-		target.classList.remove("js-product-details__size");
-		target.classList.add("js-product-details__size-active")
-	}
+    const target = event.target;
+    if(target.classList.contains('js-product-details__size')){
+        const active = document.querySelector('.js-product-details__size-active');
+        ChangeBorder(active ,'size', false);
+        ChangeBorder(target ,'size', true);
+    }
+    if(target.classList.contains('js-product-details__color')){
+        const active = document.querySelector('.js-product-details__color-active');
+        ChangeBorder(active ,'color', false);
+        ChangeBorder(target ,'color', true);
+        const color = target.dataset.value;
+        const image = 'img/tshirts/tshirt_' + color + '.jpg';
+        document.getElementById('tshirt').src = image;
+    }
 });
+function ChangeBorder(element , type , value){
+    if (type === 'size'){
+        if(value === false){
+            element.classList.remove('js-product-details__size-active');
+            element.classList.add('js-product-details__size');
+        }else{
+            element.classList.remove('js-product-details__size');
+            element.classList.add('js-product-details__size-active');
+        }
+    }
+    if (type === 'color'){
+        if(value === false){
+            element.classList.remove('js-product-details__color-active');
+            element.classList.add('js-product-details__color');
+        }else{
+            element.classList.remove('js-product-details__color');
+            element.classList.add('js-product-details__color-active');
+        }
+    }
+}
